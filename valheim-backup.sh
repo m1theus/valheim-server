2f16f70d00e866257fbf31826a02dd7400b2de59
Valheim Dedicated Server <m1theus@valheim.dedicated.server>
2021-03-28 01:58:49 +0000

    BACKUP: midas-hell-2021-03-28 01-58-49

--- valheim-backup.sh
+++ valheim-backup.sh
@@ -3,11 +3,11 @@ set -x
 
 BACKUP_NAME="midas-hell-$(date +"%Y-%m-%d %H-%M-%S")"
 
-
 cd ~/Desktop/valheim-dedicated-server
 
-cp -r AppData/LocalLow/IronGate/Valheim/worlds/* ~/Desktop/valheim-dedicated-server
+rm -r midas-hell-2021*
 
+cp -r AppData/LocalLow/IronGate/Valheim/worlds/* ~/Desktop/valheim-dedicated-server
 
 zip a "$BACKUP_NAME".zip *
 
